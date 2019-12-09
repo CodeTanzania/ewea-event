22eded902d7c632834bafbdd612dbd635937edf3
lykmapipo <lallyelias87@gmail.com>
2019-12-09 15:58:05 +0300

    feat(schema): add questio to base

--- src/schema/base.schema.js
+++ src/schema/base.schema.js
@@ -189,8 +189,8 @@ export const action = {
 /**
  * @name indicator
  * @alias indicator
- * @description Define measure used to assess need, effects
- * and situation an event.
+ * @description Define measure used to assess need, effects, situation
+ * and characteristics of an event.
  *
  * @memberof Event
  * @memberof ChangeLog
@@ -234,10 +234,57 @@ export const indicator = {
 };
 
 /**
+ * @name question
+ * @alias question
+ * @description Define a question used to assess need, effects, situation
+ * and characteristics of an event.
+ *
+ * @memberof Event
+ * @memberof ChangeLog
+ *
+ * @type {object}
+ * @property {object} type - schema(data) type
+ * @property {boolean} required - mark required
+ * @property {boolean} index - ensure database index
+ * @property {boolean} exists - ensure ref exists before save
+ * @property {object} autopopulate - auto populate(eager loading) options
+ * @property {boolean} taggable - allow field use for tagging
+ * @property {boolean} exportable - allow field use for exporting
+ * @property {boolean} aggregatable - allow field use for aggregation
+ * @property {boolean} default - default value set when none provided
+ * @property {object} fake - fake data generator options
+ *
+ * @author lally elias <lallyelias87@gmail.com>
+ * @since 0.1.0
+ * @version 0.1.0
+ * @instance
+ * @example
+ * {
+ *   _id: '5dde6ca33631a92c2d616298',
+ *   strings: { name: { en: 'Houses Damaged' } },
+ * }
+ */
+export const question = {
+  type: ObjectId,
+  ref: Predefine.MODEL_NAME,
+  // required: true,
+  index: true,
+  exists: true,
+  autopopulate: PREDEFINE_OPTION_AUTOPOPULATE,
+  taggable: true,
+  exportable: {
+    format: v => get(v, 'strings.name.en'),
+    default: 'NA',
+  },
+  aggregatable: { unwind: true },
+  default: undefined,
+};
+
+/**
  * @name unit
  * @alias unit
- * @description Define unit of measure of an event need, effects
- * or situation.
+ * @description Define unit of measure of an event need, effects, situation
+ * or characteristics.
  *
  * @memberof Event
  * @memberof ChangeLog
